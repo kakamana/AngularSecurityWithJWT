981e5b505d6b10c0c478c7f4ae70d7ce7772bc97
Muhammad Asad Kamran <asad@kamilinx.com>
2019-04-19 01:11:51 +0500

    push PTC
    push PTC

---
+++ PTC/src/app/security/app-user-auth.ts
@@ -0,0 +1,9 @@
+import { AppUserClaim } from "./app-user-claim";
+
+export class AppUserAuth {
+    userName: string = "";
+    bearerToken: string = "";
+    isAuthenticated: boolean = false;
+    claims: AppUserClaim[] = [];
+  }
+  
